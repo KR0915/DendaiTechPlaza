885579c65445f44d5420271ea1bac7d1f20af9fd
wadoyoka <atsushi021028@gmail.com>
2024-11-18 02:44:42 +0900

    人気投稿データと最近の投稿データを取得したりした

---
+++ frontend/app/logout/page.tsx
@@ -0,0 +1,7 @@
+import SignOutButton from "@/components/elements/Buttons/SignOutButton/SignOutButton";
+
+export default function logout(){
+    return(
+        <SignOutButton></SignOutButton>
+    );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,92 @@
-'use client'
-import { Button } from "@/components/ui/button";
-import { signOut } from "next-auth/react";
-
+'use client';
+import { Post, PostResponse } from "@/types/post";
+import { getPopularPosts, getRecentPosts } from "@/utils/dendaitech/Post/GET/PostGET";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const handleSignOut = () => {
-    signOut({ callbackUrl: '/signin' })
+  const [recentPosts, setRecentPosts] = useState<Post[]>([]);
+  const [popularPosts, setPopularPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+
+  useEffect(() => {
+    async function fetchRecentPosts() {
+      try {
+        const fetchedPosts: PostResponse = await getRecentPosts(0, 20);
+        setRecentPosts(fetchedPosts.content);
+        setIsLoading(false);
+      } catch (err: unknown) {
+        const errorMessage = err instanceof Error ? err.message : 'An unknown error occurred';
+        setError(errorMessage);
+        setIsLoading(false);
+      }
+    }
+
+    async function fetchPopularPosts() {
+      try {
+        const fetchedPosts: PostResponse = await getPopularPosts(0, 20);
+        setPopularPosts(fetchedPosts.content);
+        setIsLoading(false);
+      } catch (err: unknown) {
+        const errorMessage = err instanceof Error ? err.message : 'An unknown error occurred';
+        setError(errorMessage);
+        setIsLoading(false);
+      }
+    }
+
+    fetchRecentPosts();
+    fetchPopularPosts();
+  }, [])
+
+
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
-    <Button onClick={handleSignOut}>SignOut</Button>
+    <>
+
+      <div>
+        <h1>最近の投稿</h1>
+        {recentPosts.map(post => (
+          <div key={post.postId} className="post">
+            <h3>{post.title}</h3>
+            <p>{post.description}</p>
+            <p>年度: {post.year}</p>
+            <p>学科: {post.departmentName}</p>
+            <p>学年: {post.grade}</p>
+            <p>学期: {post.semester}</p>
+            <p>いいね数: {post.likesCount}</p>
+            <p>投稿者: {post.username}</p>
+            <p>投稿日: {new Date(post.createdAt).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-20">
+        <h1>人気の投稿</h1>
+        {popularPosts.map(post => (
+          <div key={post.postId} className="post">
+            <h3>{post.title}</h3>
+            <p>{post.description}</p>
+            <p>年度: {post.year}</p>
+            <p>学科: {post.departmentName}</p>
+            <p>学年: {post.grade}</p>
+            <p>学期: {post.semester}</p>
+            <p>いいね数: {post.likesCount}</p>
+            <p>投稿者: {post.username}</p>
+            <p>投稿日: {new Date(post.createdAt).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+
+    </>
+
   );
 }
