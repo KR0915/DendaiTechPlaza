4ed7f4e78643b502087029841c94c9fed3653945
dot-info <dot.info.jp@gmail.com>
2024-11-21 18:50:27 +0900

    ブックマークエラー

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,28 @@
-'use client';
-import { Post, PostResponse } from "@/types/post";
-import { getPopularPosts, getRecentPosts } from "@/utils/dendaitech/Post/GET/PostGET";
-import { useEffect, useState } from "react";
+"use client";
 
+import PostCard from "@/components/elements/PstCard/PostCard";
+import { Post, PostResponse } from "@/types/post";
+import {
+  getPopularPosts,
+  getRecentPosts,
+} from "@/utils/dendaitech/Post/GET/PostGET";
+import { useEffect, useState } from "react";
 export default function Home() {
   const [recentPosts, setRecentPosts] = useState<Post[]>([]);
   const [popularPosts, setPopularPosts] = useState<Post[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-
   useEffect(() => {
     async function fetchRecentPosts() {
       try {
         const fetchedPosts: PostResponse = await getRecentPosts(0, 20);
+        console.log(fetchedPosts);
         setRecentPosts(fetchedPosts.content);
         setIsLoading(false);
       } catch (err: unknown) {
-        const errorMessage = err instanceof Error ? err.message : 'An unknown error occurred';
+        const errorMessage =
+          err instanceof Error ? err.message : "An unknown error occurred";
         setError(errorMessage);
         setIsLoading(false);
       }
@@ -29,7 +34,8 @@ export default function Home() {
         setPopularPosts(fetchedPosts.content);
         setIsLoading(false);
       } catch (err: unknown) {
-        const errorMessage = err instanceof Error ? err.message : 'An unknown error occurred';
+        const errorMessage =
+          err instanceof Error ? err.message : "An unknown error occurred";
         setError(errorMessage);
         setIsLoading(false);
       }
@@ -37,9 +43,7 @@ export default function Home() {
 
     fetchRecentPosts();
     fetchPopularPosts();
-  }, [])
-
-
+  }, []);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -48,29 +52,20 @@ export default function Home() {
   if (error) {
     return <div>Error: {error}</div>;
   }
-
   return (
-    <>
-      <div>
-        <h1>最近の投稿</h1>
-        {recentPosts.map(post => (
-          <div key={post.postId} className="post">
-            <h3>{post.title}</h3>
-            <p>{post.description}</p>
-            <p>年度: {post.year}</p>
-            <p>学科: {post.departmentName}</p>
-            <p>学年: {post.grade}</p>
-            <p>学期: {post.semester}</p>
-            <p>いいね数: {post.likesCount}</p>
-            <p>投稿者: {post.username}</p>
-            <p>投稿日: {new Date(post.createdAt).toLocaleDateString()}</p>
-          </div>
-        ))}
+    <div className="bg-slate-200">
+      <div className="space-y-2 p-8 max-w-4xl mx-auto">
+        <h1 className="text-2xl font-bold mb-6">最近の投稿</h1>
+        <div>
+          {recentPosts.map(post => (
+            <PostCard post={post} />
+          ))}
+        </div>
       </div>
 
       <div className="mt-20">
         <h1>人気の投稿</h1>
-        {popularPosts.map(post => (
+        {/* {popularPosts.map(post => (
           <div key={post.postId} className="post">
             <h3>{post.title}</h3>
             <p>{post.description}</p>
@@ -82,10 +77,8 @@ export default function Home() {
             <p>投稿者: {post.username}</p>
             <p>投稿日: {new Date(post.createdAt).toLocaleDateString()}</p>
           </div>
-        ))}
+        ))} */}
       </div>
-
-    </>
-
+    </div>
   );
 }
